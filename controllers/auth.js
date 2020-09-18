02fde9af6fb4682827121e945d51de01dddc8415
Beatrix Woo <beatrixwho@gmail.com>
2020-09-18 14:04:41 -0700

    register routes established

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,13 +5,39 @@ const bcrypt = require("bcryptjs");
 
 /* BASE PATH */
 // REGISTER FORM
+router.get("/register", (req, res) => {
+    res.render("auth/register");
+})
+
 
 // REGISTER POST
+router.post("/register", async (req, res) => {
+    try {
+        const foundUser = await db.User.findOne({ email: req.body.email });
+        if(foundUser){
+            return res.send({ message: "Account is already registered"});
+        }
+        const salt = await bcrypt.genSalt(10);
+
+        const hash = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hash;
+
+        await db.User.create(req.body);
+    }
+})
+
 
 // LOGIN FORM
 
+
+
 // LOGIN POST <- AUTHENTICATION
 
+
+
 // LOGOUT / DELETE SESSION
+
+
+
 
 module.exports = router;
