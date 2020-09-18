01e66999f4eb0aeaa53b80f1308217fc05db6f22
Beatrix Woo <beatrixwho@gmail.com>
2020-09-18 14:22:18 -0700

    created auth views and login routes in auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,17 +5,17 @@ const bcrypt = require("bcryptjs");
 
 /* BASE PATH */
 // REGISTER FORM
-router.get("/register", (req, res) => {
-    res.render("auth/register");
-})
+router.get("/signup", (req, res) => {
+    res.render("auth/signup");
+});
 
 
 // REGISTER POST
-router.post("/register", async (req, res) => {
+router.post("/signup", async (req, res) => {
     try {
-        const foundUser = await db.User.findOne({ email: req.body.email });
-        if(foundUser){
-            return res.send({ message: "Account is already registered"});
+        const foundUser = await db.User.findOne({ username: req.body.username });
+        if(foundUser){                                                                        // if username exists
+            return res.send({ message: "Username exists"});
         }
         const salt = await bcrypt.genSalt(10);
 
@@ -23,16 +23,45 @@ router.post("/register", async (req, res) => {
         req.body.password = hash;
 
         await db.User.create(req.body);
+
+        res.redirect("/login");                                                             // redirect to login
+    } catch(error) {
+        res.redirect("404")                                                                 //redirect to 404 Page if there is an error
     }
-})
+});
 
 
 // LOGIN FORM
-
+router.get("/login", (req, res) => {
+    res.render("auth/login");
+});
 
 
 // LOGIN POST <- AUTHENTICATION
+router.post("/login", async (req, res) => {
+    try {
+        const foundUser = await db.User.findOne({ username: req.body.username });
 
+        if(!foundUser){                                                                     // if user does not exist => error
+            return res.send({message: "User does not exist"});
+        }
+
+        const match = await bcrypt.compare(req.body.password, foundUser.password);          // if user exist then compare login info with db
+
+        if(!match){                                                                         //if login info doesn't match db
+            return res.send({message: "Incorrect login info"})
+        }
+
+        req.session.currentUser = {                                                         // if login info match, create session for authentication
+            username: foundUser.usernman,
+            id: foundUser._id,
+        }
+
+        res.redirect("/")                                                                   // redirect to home after loggedin
+    } catch(err) {
+        res.send({ message: "Internal Server Error", err: err });
+    }
+})
 
 
 // LOGOUT / DELETE SESSION
