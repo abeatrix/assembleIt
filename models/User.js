71f0edd2b9c7807c74b61ad3637f0231a7c3596f
Beatrix Woo <beatrixwho@gmail.com>
2020-09-18 14:40:21 -0700

    User Model created

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {type: String, required: [true, 'you must provide a username']},
+        password: {type: String, required: true},
+        email: {type: String, required: true},
+        karma: {type: Number, default: 0},
+        posts: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Post",
+            },
+        ],
+    },
+    {
+        timestamps: true,
+        createdAt: "signupAt",
+    }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
