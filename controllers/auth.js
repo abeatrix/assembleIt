3fa436cdfc3475c71bdf8ccdc7f6b7b476ce03c0
Beatrix Woo <beatrixwho@gmail.com>
2020-09-18 14:00:30 -0700

    boilerplate created for auth.js in controller

---
+++ controllers/auth.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
+const bcrypt = require("bcryptjs");
+
+/* BASE PATH */
+// REGISTER FORM
+
+// REGISTER POST
+
+// LOGIN FORM
+
+// LOGIN POST <- AUTHENTICATION
+
+// LOGOUT / DELETE SESSION
+
+module.exports = router;
