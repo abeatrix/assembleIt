50cefde6f4e9ccb7fc41fafaf25fddb3d0f8e7f9
Beatrix Woo <beatrixwho@gmail.com>
2020-09-18 21:53:08 -0700

    linked user to posts when submitting posts on front page

--- models/User.js
+++ models/User.js
@@ -2,21 +2,21 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
     {
-        username: {type: String, required: [true, 'you must provide a username']},
+        username: {type: String, required: [true, 'you must provide a username.'] },
         password: {type: String, required: true},
-        //confirmedPassword: {type: String, required: true},
         email: {type: String, required: true},
         karma: {type: Number, default: 0},
         posts: [
+            // reference
             {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Post",
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "Post",
             },
-        ],
+          ],
     },
     {
         timestamps: true,
-        createdAt: "signupAt",
+        createdAt: "signupAt"
     }
 );
 
