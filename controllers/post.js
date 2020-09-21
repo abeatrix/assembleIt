f36defc53716f5739ae40ae09b12fcce6d790c4b
JohnDeliso <J_Deliso@yahoo.com>
2020-09-21 11:26:49 -0400

    fixed delete post route

--- controllers/post.js
+++ controllers/post.js
@@ -23,7 +23,7 @@ router.get("/newPost", (req, res) => {
   try {
     const context = {
       user: req.session.currentUser,
-  }
+    }
     res.render("posts/new.ejs", context);
   } catch (error) {
     res.send({ message: "Internal server error" });
@@ -37,7 +37,7 @@ router.get("/:id/newComment", async (req, res) => {
     const context = {
       user: req.session.currentUser,
       post: postID
-  }
+    }
     res.render("comments/new.ejs", context);
   } catch (error) {
     res.send({ message: "Internal server error" });
@@ -85,14 +85,15 @@ router.get("/:id", async (req, res) => {
   try {
     const foundPost = await db.Post.findById(req.params.id);
     const foundComments = [];
-    for(let i = 0; i < foundPost.comments.length; i++){
+    for (let i = 0; i < foundPost.comments.length; i++) {
       foundComments.push(await db.Comment.findById(foundPost.comments[i]));
     }
 
-    const context = { post: foundPost,
+    const context = {
+      post: foundPost,
       user: req.session.currentUser,
       comments: foundComments
-     };
+    };
     res.render("posts/show", context);
   } catch (error) {
     res.send({ message: "Internal server error" });
@@ -103,9 +104,10 @@ router.get("/:id", async (req, res) => {
 router.get("/:id/edit", async (req, res) => {
   try {
     const foundPost = await db.Post.findById(req.params.id);
-    const context = { post: foundPost,
+    const context = {
+      post: foundPost,
       user: req.session.currentUser,
-     };
+    };
     res.render("posts/edit.ejs", context);
   } catch (error) {
     res.send({ message: "Internal server error" });
@@ -125,6 +127,30 @@ router.put("/:id", async (req, res) => {
 // delete
 router.delete("/:id", async (req, res) => {
   try {
+    // deletes post from OPs post history
+    const foundPost = await db.Post.findById(req.params.id);
+    const foundUser = await db.User.findById(foundPost.user);
+    for (let i = 0; i < foundUser.posts.length; i++) {
+      if (foundUser.posts[i] == req.params.id) {
+        foundUser.posts.splice(i, 1);
+        await foundUser.save();
+      }
+    }
+
+    // deletes comments from commenters history
+    const foundComments = foundPost.comments;
+    for (let i = 0; i < foundComments.length; i++) {
+      const singleComment = await db.Comment.findById(foundComments[i]);
+      const userComment = await db.User.findById(singleComment.user);
+      const length = userComment.comments.length;
+      for(let j=0; j < length; j++){
+        if(foundComments[i] == userComment.comments[j]){
+          userComment.comments.splice(j,1);
+          await userComment.save();
+        }
+      }
+    }
+
     await db.Post.findByIdAndDelete(req.params.id);
     res.redirect("/");
   } catch (error) {
